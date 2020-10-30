84b0cf04a448507d11e4d435311aa33026742a50
ycbeng <yangcheebeng@hotmail.com>
2020-10-30 16:39:40 +0800

    Laravel 8

---
+++ resources/views/insertCategory.blade.php
@@ -0,0 +1,20 @@
+
+@extends('layouts.app')
+@section('content')
+<div style="text-align:center"> 
+    <form class="subform"  method="post" action="{{ route('addCategory') }} " enctype="multipart/form-data">
+    @csrf <!-- very important if you didn't insert CSRF, it not allow submit the data-->
+    <p>
+        <label for="ID" class="label">Category ID</label>
+        <input type="text" name="ID" id="ID">
+    </p>
+    <p>
+        <label for="name" class="label">Name</label>
+        <input type="text" name="name" id="name">
+    </p>
+    <p>
+        <input type="submit" name="insert" value="Insert">
+    </p>
+    </form>
+</div>
+@endsection
