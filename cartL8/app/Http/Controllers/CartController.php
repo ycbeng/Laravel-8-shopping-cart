e5974987307a61f2cc2fb87f11089051c9abfa8f
ycbeng <yangcheebeng@hotmail.com>
2020-12-10 09:31:07 +0800

    add to cart from product detail

--- cartL8/app/Http/Controllers/CartController.php
+++ cartL8/app/Http/Controllers/CartController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use App\Models\Product; 
+use App\Models\Category;
+use App\Models\myCart;
+Use Session;
+Use Auth;
+
 
 class CartController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth');
+        
+    }
+
+    public function add(){ 
+
+        $r=request(); 
+        $addCategory=myCart::create([    
+            
+            'quantity'=>$r->quantity,             
+            'orderID'=>'',
+            'productID'=>$r->id,                 
+            'userID'=>Auth::id(), 
+                        
+        ]);
+        Session::flash('success',"Product add succesful!");        
+        Return redirect()->route('products');
+    }
 }
