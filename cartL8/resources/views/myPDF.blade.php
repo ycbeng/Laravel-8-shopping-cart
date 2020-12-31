e1b0bef4b29f541da23d00d44975d1a28106d74a
ycbeng <yangcheebeng@hotmail.com>
2020-12-31 09:49:42 +0800

    update image to PDF

--- cartL8/resources/views/myPDF.blade.php
+++ cartL8/resources/views/myPDF.blade.php
@@ -11,23 +11,25 @@
 		    <table>
 		        <thead>
 		        <tr>
-		            <th>ID</th>                    
+		            <th>ID</th>  
+                    <th>Image</th>                  
 		            <th>Name</th>
                     <th>Category</th>
 		            <th>Quantity</th>
 		            <th>Price</th>
-                    <th>Action</th>
+                    
 		        </tr>
 		    </thead>
 		        <tbody>	
                 @foreach($products as $product)
 		            <tr>
 		                <td>{{$product->id}}</td>
+                        <td><img src="{{ public_path('images/'.$product->image) }}" alt="" width="50"></td>
                         <td >{{$product->name}}</td>
 		                <td>{{$product->categoryID}}</td>
                         <td>{{$product->quantity}}</td>
                         <td>{{$product->price}}</td>
-		                <td>&nbsp;</td>
+		                
 		            </tr> 
                 @endforeach				
 		        </tbody>
